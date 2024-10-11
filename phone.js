f0fb447cf0441407308fe44ec170ebfa938b2872
Mimi-Afsana <afsanamimi.wd@gmail.com>
2024-10-11 04:10:18 -0700

    Added Show all

--- phone.js
+++ phone.js
@@ -1,18 +1,41 @@
-const loadPhones = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
-    const data = await res.json();
-    const phones = data.data;
-    dispalyPhones(phones);
+const loadPhones = async (searchText, isShowAll) => {
+  loadingSpinner(true)
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+  const data = await res.json();
+  const phones = data.data;
+  dispalyPhones(phones, isShowAll);
+
 }
 
-const dispalyPhones = (phones) => {
-    // console.log(phones)
-    const phoneContainer = document.getElementById('phone-container')
-    phones.forEach(phone => {
-        const phoneCard = document.createElement('div')
-        phoneCard.classList = `card bg-base-100 shadow-xl`
-        phoneCard.innerHTML =
-            `
+const dispalyPhones = (phones, isShowAll) => {
+  // console.log(phones)
+  const phoneContainer = document.getElementById('phone-container')
+  // clear before search result
+  phoneContainer.textContent = ''
+
+
+  if (phones.length > 12 && !isShowAll) {
+    shoAll()
+  }
+  else {
+    shoAllhidded()
+  }
+
+  console.log('show all result', isShowAll)
+
+  // 10ta phone dekhabe
+  console.log(phones.length)
+
+
+  if (!isShowAll) {
+    phones = phones.slice(0, 12)
+  }
+
+  phones.forEach(phone => {
+    const phoneCard = document.createElement('div')
+    phoneCard.classList = `card bg-base-100 p-4 shadow-xl `
+    phoneCard.innerHTML =
+      `
         <figure class="px-10 pt-10">
                       <img
                         src="${phone.image}"
@@ -27,10 +50,51 @@ const dispalyPhones = (phones) => {
                       </div>
                     </div>
         `;
-        phoneContainer.appendChild(phoneCard)
+    phoneContainer.appendChild(phoneCard)
 
-    });
+  });
+  //  hide loading spinner
+  loadingSpinner(false)
+}
+
+// showall phones implements
+const shoAll = () => {
+  const show = document.getElementById('show-all')
+  show.classList.remove("hidden");
+}
+
+// add hidden
+const shoAllhidded = () => {
+  const show = document.getElementById('show-all')
+  show.classList.add("hidden");
+}
+
+
+// handle search field
+const handleSearch = (isShowAll) => {
+  // console.log("clicked")
+  const searchField = document.getElementById('search-input')
+  const searchTextValue = searchField.value
+  loadPhones(searchTextValue, isShowAll)
 
 }
 
-loadPhones();
+
+// loading spinner implement
+const loadingSpinner = (isLoading) => {
+  const loading = document.getElementById('loading-spinner')
+  if (isLoading) {
+    loading.classList.remove('hidden')
+  }
+  else {
+    loading.classList.add('hidden')
+  }
+}
+
+
+// handle showAll
+const handleShowAll = () => {
+  // const showAll = document.getElementById('show-all')
+  // console.log(showAll, 'clicked')
+  handleSearch(true)
+}
